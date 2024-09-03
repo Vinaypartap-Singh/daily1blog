8925d0066004e6de318ddb4d59d0b04232e2070c
Vinay <developervsandhu@gmail.com>
2024-09-03 02:15:18 -0400

    sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,21 +44,21 @@ export default function Header() {
   ];
 
   return (
-    <div>
-      <header class="text-gray-600 body-font sticky top-0 bg-black hidden lg:block">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center max-width-content">
+    <div className="sticky top-0 z-50">
+      <header className="text-gray-600 body-font sticky top-0 bg-black hidden lg:block">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center max-width-content">
           <Link
             to={"/"}
-            class="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+            className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
           >
-            <span class="ml-3 text-xl">Daily 1 Blog</span>
+            <span className="ml-3 text-xl">Daily 1 Blog</span>
           </Link>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             {navItems.map((item) => {
               return (
                 <Link
                   to={item.link}
-                  class="text-white mr-5 hover:text-white"
+                  className="text-white mr-5 hover:text-white"
                   key={item.name}
                 >
                   {item.name}
@@ -71,19 +71,19 @@ export default function Header() {
             <>
               <Link
                 to={"/createauthorprofile"}
-                class="mr-5 inline-flex text-white hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
+                className="mr-5 inline-flex text-white hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
               >
                 Create Profile
               </Link>
               <Link
                 to={"/postblog"}
-                class="mr-5 inline-flex border border-orange-400 bg-orange-400 text-white hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
+                className="mr-5 inline-flex border border-orange-400 bg-orange-400 text-white hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
               >
                 Post a Blog
               </Link>
               <Link
                 onClick={logUserOut}
-                class="inline-flex border border-orange-400 text-orange-400 hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
+                className="inline-flex border border-orange-400 text-orange-400 hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
               >
                 LogOut
               </Link>
@@ -94,14 +94,14 @@ export default function Header() {
             <>
               <Link
                 to={"/login"}
-                class="mr-2 inline-flex text-orange-400 items-center border-0 py-1 px-3 focus:outline-none hover:bg-orange-600 hover:text-white rounded mt-4 md:mt-0"
+                className="mr-2 inline-flex text-orange-400 items-center border-0 py-1 px-3 focus:outline-none hover:bg-orange-600 hover:text-white rounded mt-4 md:mt-0"
               >
                 Login
               </Link>
 
               <Link
                 to={"/signup"}
-                class="inline-flex border border-orange-400 text-orange-400 hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
+                className="inline-flex border border-orange-400 text-orange-400 hover:border-transparent items-center py-1 px-3 hover:bg-orange-600 hover:text-white rounded text-base mt-4 md:mt-0"
               >
                 Create an Account
               </Link>
