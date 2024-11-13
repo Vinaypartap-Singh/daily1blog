ae2df530a05b4f378293c938da80464b2389f522
Vinay Sandhu <vinaysandhu@Vinays-Mac-mini.local>
2024-11-12 19:37:44 -0500

    Footer Logo Removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ export default function Footer() {
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <img src={Logo} className={"w-14 h-14 object-fit"} />
           <span className="ml-3 text-xl">Daily 1 Blog</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
