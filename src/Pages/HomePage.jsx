074107d835ab84a1a092f2943934ab563c57733f
Vinay Developer <developervsandhu@gmail.com>
2025-07-02 10:03:56 -0400

    Section Added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import BlogsSection from "../components/sections/BlogsSection";
+import HeroBlog from "../components/sections/HeroBlog";
 
 export default function HomePage() {
   return (
     <div>
-      {/* <HeroBlog /> */}
+      <HeroBlog />
       {/* <CategorySection showHeader={true} /> */}
       <BlogsSection showHeader={true} />
     </div>
